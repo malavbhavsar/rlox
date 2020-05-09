d7bcfebfdcbf3086389fc1834758bfc75dbcec41
Malav Bhavsar <malav@squareup.com>
2020-05-09 14:20:34 -0700

    Add error handlling infrastructure

--- lib/rlox.rb
+++ lib/rlox.rb
@@ -1,18 +1,17 @@
 # frozen_string_literal: true
 
 require 'readline'
+require 'singleton'
 require File.expand_path("../scanner", __FILE__)
 
 class Rlox
-  def self.run_file(file)
-    file_content = File.read file
-    run(file_content)
-  end
+  class HadError
+    include Singleton
 
-  def self.run_prompt
-    while true
-      buffer = Readline.readline("> ", true)
-      run(buffer)
+    attr_accessor :value
+
+    def initialize
+      @value = false
     end
   end
 
@@ -20,8 +19,33 @@ class Rlox
     puts "# Usage: rlox [FILE]"
   end
 
-  def self.run(code)
+  def self.run_file(file)
+    file_content = File.read file
+    run(file_content)
+
+    # Error in code
+    exit(65) if HadError.instance.value
+  end
+
+  def self.run_prompt
+    while true
+      buffer = Readline.readline("> ", true)
+      run(buffer)
+      HadError.instance.value = false
+    end
+  end
+
+  private_class_method def self.run(code)
     scanner = Scanner.new(code)
     scanner.scan_tokens.each { |token| p token }
+  end
+
+  private_class_method def self.error(line, message)
+    report(line, "", message)
+  end
+
+  private_class_method def self.report(line, where, message)
+    p "[line #{line}] Error #{where}: #{message}"
+    HadError.instance.value = true
   end
 end
